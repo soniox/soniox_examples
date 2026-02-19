d12bb737e6d43fb648b2d03dd762f2e53c97f6f7
Slava Obukhov <slava.obukhov@soniox.com>
2026-02-19 12:52:10 +0100

    Bring removed comments back

--- speech_to_text/apps/soniox-live-demo/react/src/renderers/renderer.tsx
+++ speech_to_text/apps/soniox-live-demo/react/src/renderers/renderer.tsx
@@ -15,6 +15,9 @@ interface RendererProps {
   placeholder: string;
 }
 
+// Component for pretty displaying tokens. It adds label for different speakers
+// and label for different languages. It also visually displays difference
+// between final and non-final tokens.
 export default function Renderer({ tokens, placeholder }: RendererProps) {
   let lastSpeaker: string | undefined;
   let lastLanguage: string | undefined;
@@ -27,14 +30,7 @@ export default function Renderer({ tokens, placeholder }: RendererProps) {
       ) : (
         <div>
           {tokens.map((token, idx) => {
-            if (token.text === "<end>") {
-              return (
-                <span
-                  key={`end-token-${idx}`}
-                  className="text-gray-400 italic text-xs"
-                >{` <end>`}</span>
-              );
-            }
+            // Track speaker changes to show speaker labels only when speaker changes
             const isNewSpeaker = token.speaker && token.speaker !== lastSpeaker;
             const isNewLanguage =
               token.language && token.language !== lastLanguage;
@@ -44,15 +40,19 @@ export default function Renderer({ tokens, placeholder }: RendererProps) {
 
             return (
               <React.Fragment key={`rendered-token-${idx}`}>
+                {/* Show speaker label if speaker changed or new speaker joined */}
                 {isNewSpeaker && token.speaker && (
                   <SpeakerLabel speakerNumber={token.speaker} />
                 )}
+                {/* Show new language on a new line */}
                 {isNewLanguage && !isNewSpeaker && <br />}
+                {/* Show language label if language changed or new language is detected */}
                 {isNewLanguage && (
                   <span className="text-gray-500 text-xs bg-gray-200 px-2 py-0.5 rounded-full mr-1">
                     {`${getLanguage(token.language!).name}`}
                   </span>
                 )}
+                {/* Display final and non-final tokens with different colors */}
                 <span
                   className={token.is_final ? "text-gray-900" : "text-gray-500"}
                 >
